42f2d396b0049e0dda7ef325b76a2556f218a192
raresopariuc <raresopariuc@users.noreply.github.com>
2018-04-25 20:17:14 +0300

    Made web application

--- src/main/java/org/bootcamp/MainApp.java
+++ src/main/java/org/bootcamp/MainApp.java
@@ -1,50 +1,15 @@
 package org.bootcamp;
 
-import org.bootcamp.service.InsuranceCalculationResult;
-import org.bootcamp.service.InsuranceCalculatorService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-public class MainApp implements CommandLineRunner {
-
-    private final InsuranceCalculatorService service;
-
-    public MainApp(InsuranceCalculatorService service) {
-        this.service = service;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        final List<InsuranceCalculationResult> resultList1 = service.calculateAll();
-        final List<InsuranceCalculationResult> resultList2 = service.getCostsHigherThan(1000);
-
-        resultList1.forEach(MainApp::printCalculationResult);
-
-        System.out.println();
-
-        resultList2.forEach(MainApp::printCalculationResult);
-
-        System.out.println();
-
-        printCalculationResult(service.calculateById("3c997def-3cff-11e8-c243-14de190f32bc"));
-    }
-
-    private static final String OUTPUT_FORMAT = "%s with id %s has total cost %.2f";
-
+@Controller
+@ResponseBody
+public class MainApp {
     public static void main(String[] args) {
         SpringApplication.run(MainApp.class, args);
-    }
-
-
-    private static void printCalculationResult(InsuranceCalculationResult result) {
-
-        if (result != null) {
-            final String output = String.format(OUTPUT_FORMAT, result.getVehicleTypeName(), result.getId(), result.getCost());
-            System.out.println(output);
-        }
     }
 }
